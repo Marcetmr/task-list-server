df93050fd665ec2952f4d20d71c6be0b25e70eb7
Marcela Tatiana Martínez <marce.t1994@gmail.com>
2023-11-03 16:36:25 -0600

    Servidor que retorna en JSON un array de tareas

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('It\'s working')
-})
+const tasks = [
+    {id: 1, isCompleted: false, description: 'Limpiar los vidrios'},
+    {id: 2, isCompleted: false, description: 'Cocinar el almuerzo'},
+    {id: 3, isCompleted: true, description: 'Regar las plantas'},
+    {id: 4, isCompleted: false, description: 'Hacer ejercicio'},
+];
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
 
 app.listen(PORT, () => {
     console.log(`La aplicación está escuchando en el puerto ${PORT}`);
